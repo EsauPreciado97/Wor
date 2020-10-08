33124d1f4fe3d12f2abc029acc13fafe2b122fc9
esaupreciado97 <esauplm@gmail.com>
2020-10-08 10:59:59 -0500

    Changes

--- php/send.php
+++ php/send.php
@@ -1,19 +1,21 @@
 <?php
-if(isset($_POST["nombre"]) && isset($_POST["tel"]) && isset($_POST["ciudad"]) && isset($_POST["empresa"]) && isset($_POST["distribuidor"])  
+if(isset($_POST["nombre"]) && isset($_POST["tel"]) && isset($_POST["email"]) && isset($_POST["empresa"]) && isset($_POST["sede"]) && isset($_POST["size"]) 
 
 {
-$to = "phenomenadevelopment@gmail.com";
+$to = "info@wor.mx";
 $subject = "Mensaje Enviado";
 $contenido .= "Nombre: ".$_POST["nombre"]."\n";
 $contenido .= "Telefono: ".$_POST["tel"]."\n\n\n";
-$contenido .= "Ciudad: ".$_POST["ciudad"]."\n";
+$contenido .= "Email: ".$_POST["email"]."\n";
 $contenido .= "Empresa: ".$_POST["empresa"]."\n";
-$contenido .= "¿Te interesa ser distribuidor?: ".$_POST["distribuidor"]."\n";
-$header = "From: mercadotecnia@packlife.mx\nReply-To:".$_POST["email"]."\n";
+$contenido .= "Sede de interes: ".$_POST["sede"]."\n";
+$contenido .= "Numero de empleados: ".$_POST["size"]."\n";
+
+$header = "From: info@wor.mx\nReply-To:".$_POST["email"]."\n";
 $header .= "Mime-Version: 1.0\n";
 $header .= "Content-Type: text/plain";
-if(mail($to, $subject, $contenido ,$header)){
-echo '<script language="javascript">alert("¡Gracias! Hemos recibido tu información. Muy pronto uno de nuestros asesores se pondrá en contacto contigo.");window.location.href="index.php"</script>';
+if(mail($to, $subject, $contenido, $header)){
+echo '<script language="javascript">alert("¡Gracias! Hemos recibido tu información. Muy pronto uno de nuestros asesores se pondrá en contacto contigo.");window.location.href="../index.html"</script>';
 }
 }
 ?>
